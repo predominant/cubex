5b7b329d7b6c3dee9be0478f909ec298cda5760e
Brooke.bryan <brooke@bajb.net>
2012-11-21 20:26:16 +0000

    controller updates for new responses

--- application/simple/controller/basic.php
+++ application/simple/controller/basic.php
@@ -11,11 +11,4 @@ namespace Cubex\Application\Simple;
 class basicController extends \Cubex\Base\Controller
 {
 
-  public function runPage($path)
-  {
-    echo $this->processedRoute();
-    echo $path;
-    echo \id(new \Cubex\Base\WebPage())->setTitle("Simple Application : Basic")->render();
-    echo "Basic";
-  }
 }
--- application/simple/controller/default.php
+++ application/simple/controller/default.php
@@ -13,7 +13,9 @@ class defaultController extends \Cubex\Base\Controller
 
   public function runPage()
   {
-    echo \id(new \Cubex\Base\WebPage())->setTitle("Simple Application")->render();
+    $webpage = new \Cubex\Base\WebPage();
+    $webpage->setTitle("Simples Application");
+    $webpage->beginCapture();
 
     $partial = new \Cubex\View\Partial(
       '<div><strong>{#name}</strong> - {#title}</div>', array("name", "title")
@@ -22,12 +24,17 @@ class defaultController extends \Cubex\Base\Controller
     $partial->addElement("Gareth Evans", "Director of Developer");
     echo $partial->render();
 
-    /*$user = new \Cubex\Module\User\User();
+    $partial = new \Cubex\View\Partial('<div><strong>%s</strong>: %s</div>');
+    $partial->addElement("Brooke Bryan", "CTO");
+    $partial->addElement("Gareth Evans", "Director of Developer");
+    echo $partial->render();
+
+    $user = new \Cubex\Module\User\User();
     $user->authenticate('user','password');
 
     echo "User Module Version: " . $user->moduleVersion() . "\n<br/>";
     echo "User Module Name: " . $user->moduleName() . "\n<br/>";
-    echo "User Module Description: " . $user->moduleDescription() . "\n<br/>";*/
+    echo "User Module Description: " . $user->moduleDescription() . "\n<br/>";
 
     $usr       = new \Cubex\Module\User\Model\User();
     $usr->name = ' Brooke ';
@@ -56,5 +63,8 @@ class defaultController extends \Cubex\Base\Controller
     $menu->addItem("Link 2", '/link2');
     echo $menu->render();
 
+    $redirect = new \Cubex\Http\Redirect('http://www.google.com');
+
+    new \Cubex\Http\Response($webpage);
   }
 }
