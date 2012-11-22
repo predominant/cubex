08793b3a4c3089f9023ea0f0b47aa5935df3efe8
Gareth Evans <garoevans@gmail.com>
2012-11-22 18:57:20 +0000

    Test successful Application Initiator

--- __tests__/base/ApplicationTest.php
+++ __tests__/base/ApplicationTest.php
@@ -15,4 +15,16 @@ class Base_ApplicationTest extends PHPUnit_Framework_TestCase
 
     Cubex\Base\Application::initialise('unittest');
   }
+
+  public function testSimpleApplicationInitiator()
+  {
+    // We need to set the request object here as we are mocking a http request
+    // even though we're running via the cli
+    Cubex\Cubex::core()->setRequest(new Cubex\Http\Request());
+    ob_start();
+    Cubex\Base\Application::initialise('simple');
+    $response = ob_get_contents();
+    ob_end_clean();
+    $this->assertStringStartsWith('<!DOCTYPE html>', $response);
+  }
 }
