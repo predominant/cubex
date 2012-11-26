c93db0fda39ce6086210a250269a8306d8c2cfb2
bajbnet <brooke@bajb.net>
2012-11-26 09:43:50 +0000

    basic edge

---
+++ cubes/data/edge.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User: brooke
+ * Date: 26/11/12
+ * Time: 09:12
+ * Description:
+ */
+
+
+namespace Cubex\Data;
+
+abstract class Edge
+{
+  protected $_edge_type = null;
+
+  public static function edgeType()
+  {
+    return strtolower(end(explode('\\',get_called_class()))) . ':' . substr(md5(get_called_class()),22) . ':';
+  }
+
+  abstract public static function getEdges($source_id, $edge_type=null);
+
+  abstract public static function addEdge($source_id,$relation_id, $edge_type);
+}
