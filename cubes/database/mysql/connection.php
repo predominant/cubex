45ec7703cbdabae4eeb550093ece08fd890e4be6
Brooke.bryan <brooke@bajb.net>
2012-11-29 15:56:38 +0000

    mysqli start

--- cubes/database/mysql/connection.php
+++ cubes/database/mysql/connection.php
@@ -10,37 +10,56 @@ namespace Cubex\Database\MySQL;
 class Connection implements \Cubex\Database\Connection
 {
 
-  public function __construct(array $config = array())
+  /**
+   * @var \mysqli
+   */
+  protected $_connection;
+  /**
+   * @var \Cubex\Data\Handler
+   */
+  protected $_config;
+
+  public function __construct(\Cubex\Data\Handler $config)
   {
+    $this->_config = $config;
+
   }
 
-  public function connect($mode='w')
+  public function connect($mode = 'w')
   {
+    $hostname = $this->_config->getStr('hostname', 'localhost');
+    if($mode == 'r')
+    {
+      $slaves = $this->_config->getArr('slaves', array($hostname));
+      shuffle($slaves);
+      $hostname = current($slaves);
+    }
 
+    $this->_connection = new \mysqli(
+      $hostname,
+      $this->_config->getStr('username', 'root'),
+      $this->_config->getStr('password', ''),
+      $this->_config->getStr('database', 'test'),
+      $this->_config->getStr('port', 3306)
+    );
+
+    return $this;
   }
 
   public function disconnect()
   {
-
+    $this->_connection->close();
   }
 
   public function escapeColumnName($column)
   {
+    $column = str_replace('`', '', $this->escapeString($column));
+
     return "`$column`";
   }
 
   public function escapeString($string)
   {
-    return $string;
-  }
-
-  public function escapeStringForLikeClause($string)
-  {
-    return $string;
-  }
-
-  public function escapeMultilineComment($comment)
-  {
-    return $comment;
+    return $this->_connection->real_escape_string($string);
   }
 }
