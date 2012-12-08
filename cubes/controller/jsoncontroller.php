12723c0404d17c32135feaeb461773afdd1a5f4b
Brooke.bryan <brooke@bajb.net>
2012-12-08 11:13:40 +0000

    more specific controller types

---
+++ cubes/controller/jsoncontroller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: brooke.bryan
+ * Date: 08/12/12
+ * Time: 10:29
+ * Description:
+ */
+
+namespace Cubex\Controller;
+
+use Cubex\Base\Controller;
+
+abstract class JSONController extends Controller
+{
+
+}
---
+++ cubes/controller/webpagecontroller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: brooke.bryan
+ * Date: 08/12/12
+ * Time: 10:27
+ * Description:
+ */
+
+namespace Cubex\Controller;
+
+use Cubex\Base\Controller;
+
+abstract class WebpageController extends Controller
+{
+  protected $_webpage;
+}
