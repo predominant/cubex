6e517bcd7bbb45fd1b39b9ccb20840b3e36f4771
Gareth Evans <garoevans@gmail.com>
2012-12-09 16:48:18 +0000

    Added a setup.php file to be used as a bootstrap. Enables setting up of mock classes and doing any other required booting

---
+++ __tests__/setup.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * File: setup.php
+ * Date: 09/12/12
+ * Time: 16:15
+ * @author: gareth.evans
+ */
+
+require_once dirname(dirname(__FILE__)) .'/cubes/boot.php';
+
+final class Setup {
+
+}
+
+final class Application extends \Cubex\Base\Application
+{
+  public function getDefaultController()
+  {
+    return 'Controller';
+  }
+
+  public function launch()
+  {
+    \Cubex\Cubex::core()->setController(new Controller());
+  }
+}
+
+final class ApplicationFailTest extends \Cubex\Base\Application
+{
+  public function launch()
+  {
+    throw new Exception('Test Exception');
+  }
+
+  public function getName()
+  {
+    return 'ApplicationFailTest';
+  }
+}
+
+final class Controller extends \Cubex\Base\Controller
+{
+  public function processRequest()
+  {
+    $webpage = new \Cubex\Base\WebPage();
+    $webpage->setTitle("Test Application");
+
+    return new Response($webpage);
+  }
+}
+
+final class Response extends \Cubex\Http\Response
+{
+  public function render()
+  {
+    echo $this->_webpage->renderHead();
+    echo $this->_webpage->renderBody();
+    echo $this->_webpage->renderClosing();
+  }
+}
