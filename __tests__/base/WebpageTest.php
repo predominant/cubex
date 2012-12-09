092ba19d158b0aee3aa2ab1b69e8afe14ca22391
Gareth Evans <garoevans@gmail.com>
2012-12-09 16:49:30 +0000

    Updated test to work with new mock objects availabe from new boot file

--- __tests__/base/WebpageTest.php
+++ __tests__/base/WebpageTest.php
@@ -12,33 +12,19 @@ class Base_WebpageTest extends PHPUnit_Framework_TestCase
 
   public function setUp()
   {
-    if(!class_exists('Cubex\Applications\Simple\Application'))
-    {
-      return;
-    }
     Cubex\Cubex::core()->setRequest(new Cubex\Http\Request());
     ob_start();
-    Cubex\Base\Application::initialise('simple');
+    $application = new Application();
+    Cubex\Base\Application::initialise($application);
     ob_end_clean();
     $this->_webpage = new \Cubex\Base\WebPage();
-  }
-
-  private function _getWebpageObject()
-  {
-    if($this->_webpage === null)
-    {
-      $this->markTestSkipped(
-        "This test requires the cubex_example applications"
-      );
-    }
-    return $this->_webpage;
   }
 
   public function testGetController()
   {
     $this->assertInstanceOf(
       '\Cubex\Base\Controller',
-      $this->_getWebpageObject()->controller()
+      $this->_webpage->controller()
     );
   }
 }
