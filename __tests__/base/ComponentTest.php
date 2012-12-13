aff84f22fa41adbf81cb6381abe8a661f2d7d572
Gareth Evans <garoevans@gmail.com>
2012-12-13 20:16:28 +0000

    Added component test

---
+++ __tests__/base/ComponentTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * File: ComponentTest.php
+ * Date: 13/12/12
+ * Time: 19:48
+ * @author: gareth.evans
+ */
+namespace Cubex\Tests;
+use Cubex\Tests\Views\ViewTest;
+
+class Base_ComponentTest extends \PHPUnit_Framework_TestCase
+{
+  private $_component;
+
+  protected function setUp()
+  {
+    $this->_component = new Component();
+  }
+
+  public function testGetName()
+  {
+    $this->assertTrue($this->_component->getName() === '');
+  }
+
+  public function testGetDescription()
+  {
+    $this->assertTrue($this->_component->getDescription() === '');
+  }
+
+  public function testGetViewReturnsTestViewObject()
+  {
+    $this->assertEquals(
+      new ViewTest(),
+      $this->_component->getView('ViewTest')
+    );
+  }
+}
