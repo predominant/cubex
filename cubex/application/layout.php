ba2f524c46fd2ea91fa7b941aacc04d4824f8161
Brooke.bryan <brooke@bajb.net>
2012-12-21 17:56:31 +0000

    Layout render hooks

--- cubex/application/layout.php
+++ cubex/application/layout.php
@@ -20,6 +20,8 @@ class Layout extends TemplatedView
   private $_nested = array();
   private $_layout_template = 'default';
 
+  protected $_render_hooks = array('before' => array(), 'after' => array());
+
   /**
    * Set new template file for layout
    *
@@ -75,6 +77,18 @@ class Layout extends TemplatedView
   public function renderNest($name, $containDivId = true)
   {
     $rendered = '';
+
+    if(isset($this->_render_hooks['before'][$name]))
+    {
+      foreach($this->_render_hooks['before'][$name] as $renderHook)
+      {
+        if($renderHook instanceof Renderable)
+        {
+          $rendered .= $renderHook->render();
+        }
+      }
+    }
+
     if(isset($this->_nested[$name]))
     {
       $nest = $this->_nested[$name];
@@ -84,18 +98,61 @@ class Layout extends TemplatedView
       }
     }
 
-    if($containDivId === false)
-    {
-      return $rendered;
-    }
-    else
+    if($containDivId !== false)
     {
       if(\is_string($containDivId))
       {
         $name = $containDivId;
       }
 
-      return HTMLElement::create('div', array('id' => $name), $rendered)->render();
+      $rendered = HTMLElement::create('div', array('id' => $name), $rendered)->render();
     }
+
+    if(isset($this->_render_hooks['after'][$name]))
+    {
+      foreach($this->_render_hooks['after'][$name] as $renderHook)
+      {
+        if($renderHook instanceof Renderable)
+        {
+          $rendered .= $renderHook->render();
+        }
+      }
+    }
+
+    return $rendered;
+  }
+
+  /**
+   * @param                        $when
+   * @param                        $nest
+   * @param \Cubex\View\Renderable $render
+   */
+  protected function hookRender($when, $nest, Renderable $render)
+  {
+    $this->_render_hooks[$when][$nest][] = $render;
+  }
+
+  /**
+   * @param                        $nest
+   * @param \Cubex\View\Renderable $render
+   *
+   * @return Layout
+   */
+  public function nestBefore($nest, Renderable $render)
+  {
+    $this->hookRender("before", $nest, $render);
+    return $this;
+  }
+
+  /**
+   * @param                        $nest
+   * @param \Cubex\View\Renderable $render
+   *
+   * @return Layout
+   */
+  public function nestAfter($nest, Renderable $render)
+  {
+    $this->hookRender("after", $nest, $render);
+    return $this;
   }
 }
