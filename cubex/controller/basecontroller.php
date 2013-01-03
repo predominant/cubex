68be5fd750ff6342ab9c3f51835a64adc39966ff
Brooke.bryan <brooke.bryan@jdiuk.com>
2013-01-03 17:42:17 +0000

    controller request local variable

--- cubex/controller/basecontroller.php
+++ cubex/controller/basecontroller.php
@@ -106,7 +106,7 @@ abstract class BaseController implements Dispatchable, \IteratorAggregate
    */
   public function request()
   {
-    return Cubex::request();
+    return $this->_request;
   }
 
   /**
