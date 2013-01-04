85d9d6ac6dd51f2477519393185ec5ba437dba42
Gareth Evans <garoevans@gmail.com>
2013-01-04 10:09:32 +0000

    Added an wrapper for the SplEnum class, simply to let an IDE know that the construct patams aren't necessary. Also added some docs

--- cubex/type/enum.php
+++ cubex/type/enum.php
@@ -1,5 +1,13 @@
 <?php
 /**
+ * This gives us a convenient way to always have an Enum object available and
+ * utilise Spl Types if available. It does kick up a bit of a fuss in some IDEs
+ * as it sees two classes with the same name, but we know this isn't an issue as
+ * the code is down there :)
+ *
+ * We also wrap the SplEnum class to stop IDEs thinkging that the constructor
+ * paramaters are necessary.
+ *
  * File: reflection.php
  * Date: 02/01/13
  * Time: 11:16
@@ -12,7 +20,15 @@ use Cubex\Type\Enum\Reflection;
 
 if(class_exists("\\SplEnum"))
 {
-  abstract class Enum extends \SplEnum
+  class SplWrapper extends \SplEnum
+  {
+    public function __construct($enum = null, $strict = false)
+    {
+      parent::__construct($enum, $strict);
+    }
+  }
+
+  abstract class Enum extends SplWrapper
   {
 
   }
